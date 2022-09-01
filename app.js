79e9462f3e29027eeb35cb93b63500cff7f095d7
Romulo <romulosilva001@gmail.com>
2022-09-01 12:09:40 -0300

    Realizada a comunicação via mensagem do back pro front e vice-versa utilizando socket.on

--- app.js
+++ app.js
@@ -2,10 +2,22 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const app = express();
-
+const socketIO = require('socket.io');
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log(`Server running on PORT: ${process.env.PORT}`);
+})
+
+const io = socketIO(server);
+
+io.on('connection', (socket) =>{
+  console.log('New connection');
+
+  socket.emit('Hello', {message: "Seja bem vindo!"});
+
+  socket.on('hello_client_response', (data) => {
+    console.log(data.message);
+  })
 })
